9cb5e9d8bed8a284c14e2f6b0384853955764bf3
LongKun <longdh.udu.ptit@gmail.com>
2026-05-14 00:50:38 +0700

    fix loi logic

--- frontend/.umirc.ts
+++ frontend/.umirc.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "umi";
 
 export default defineConfig({
+  plugins: ["@umijs/plugins/dist/model"],
   routes: [
     { path: "/", component: "index" },
     { path: "/docs", component: "docs" },
