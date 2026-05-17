e30cb224eefb8f9198c50ec4fdaf5b142ce57a94
LeQuang <lienminhtocchien2k6@gmail.com>
2026-05-18 01:33:45 +0700

    refactor: merge fe + be

---
+++ backend/src/modules/auth/auth.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+
+@Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'jwt_secret_key_tam_thoi_cho_dev',
+      signOptions: { expiresIn: '7d' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+  exports: [AuthService],
+})
+export class AuthModule {}
